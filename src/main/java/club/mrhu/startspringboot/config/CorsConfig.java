59d5b8b19fd1a2c2bf606f8c8777e333925600ec
baboonHu <499363352@qq.com>
2020-05-07 16:46:09 +0800

    跨域配置

---
+++ src/main/java/club/mrhu/startspringboot/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package club.mrhu.startspringboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer CorsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowCredentials(true)
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .maxAge(3600);
+            }
+        };
+    }
+}
